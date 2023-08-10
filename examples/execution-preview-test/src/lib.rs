640f096bc5485532959bcc7a0c6de3c73690f670
Florian Pieper <florian@pieper.ai>
2023-08-11 00:47:40 +0200

    Proof of concept for different addresses for preview and execution receipt

--- examples/execution-preview-test/src/lib.rs
+++ examples/execution-preview-test/src/lib.rs
@@ -2,44 +2,19 @@ use scrypto::prelude::*;
 
 #[blueprint]
 mod hello {
-    struct Hello {
-        // Define what resources and data will be managed by Hello components
-        sample_vault: Vault,
-    }
+    struct Hello {}
 
     impl Hello {
-        // Implement the functions and methods which will manage those resources and data
+        pub fn instantiate_hello() -> (Global<Hello>, Bucket) {
+            let my_bucket: Bucket =
+                ResourceBuilder::new_fungible(OwnerRole::None).mint_initial_supply(1000);
 
-        // This is a function, and can be called directly on the blueprint once deployed
-        pub fn instantiate_hello() -> Global<Hello> {
-            // Create a new token called "HelloToken," with a fixed supply of 1000, and put that supply into a bucket
-            let my_bucket: Bucket = ResourceBuilder::new_fungible(OwnerRole::None)
-                .metadata(metadata! {
-                    init {
-                        "name" => "HelloToken".to_owned(), locked;
-                        "symbol" => "HT".to_owned(), locked;
-                    }
-                })
-                .mint_initial_supply(1000);
+            let component = Self {}
+                .instantiate()
+                .prepare_to_globalize(OwnerRole::None)
+                .globalize();
 
-            // Instantiate a Hello component, populating its vault with our supply of 1000 HelloToken
-            Self {
-                sample_vault: Vault::with_bucket(my_bucket),
-            }
-            .instantiate()
-            .prepare_to_globalize(OwnerRole::None)
-            .globalize()
-        }
-
-        // This is a method, because it needs a reference to self.  Methods can only be called on components
-        pub fn free_token(&mut self) -> Bucket {
-            info!(
-                "My balance is: {} HelloToken. Now giving away a token!",
-                self.sample_vault.amount()
-            );
-            // If the semi-colon is omitted on the last line, the last value seen is automatically returned
-            // In this case, a bucket containing 1 HelloToken is returned
-            self.sample_vault.take(1)
+            (component, my_bucket)
         }
     }
 }
--- examples/execution-preview-test/tests/lib.rs
+++ examples/execution-preview-test/tests/lib.rs
@@ -1,43 +1,38 @@
 use scrypto::prelude::*;
 use scrypto_unit::*;
-use transaction::builder::ManifestBuilder;
+use transaction::{builder::ManifestBuilder, prelude::PreviewFlags};
 
 #[test]
 fn test_hello() {
-    // Setup the environment
-    let mut test_runner = TestRunner::builder().build();
-
-    // Create an account
+    let mut test_runner = TestRunner::builder().without_trace().build();
     let (public_key, _private_key, account) = test_runner.new_allocated_account();
-
-    // Publish package
     let package_address = test_runner.compile_and_publish(this_package!());
 
-    // Test the `instantiate_hello` function.
     let manifest = ManifestBuilder::new()
+        .lock_standard_test_fee(account)
         .call_function(
             package_address,
             "Hello",
             "instantiate_hello",
             manifest_args!(),
         )
-        .build();
-    let receipt = test_runner.execute_manifest_ignoring_fee(
-        manifest,
-        vec![NonFungibleGlobalId::from_public_key(&public_key)],
-    );
-    println!("{:?}\n", receipt);
-    let component = receipt.expect_commit(true).new_component_addresses()[0];
-
-    // Test the `free_token` method.
-    let manifest = ManifestBuilder::new()
-        .call_method(component, "free_token", manifest_args!())
         .deposit_batch(account)
         .build();
-    let receipt = test_runner.execute_manifest_ignoring_fee(
-        manifest,
+
+    let preview_receipt = test_runner.preview_manifest(
+        manifest.clone(),
+        vec![public_key.clone().into()],
+        0,
+        PreviewFlags::default(),
+    );
+    let execution_receipt = test_runner.execute_manifest(
+        manifest.clone(),
         vec![NonFungibleGlobalId::from_public_key(&public_key)],
     );
-    println!("{:?}\n", receipt);
-    receipt.expect_commit_success();
+    assert_eq!(
+        preview_receipt.expect_commit(true).new_resource_addresses(),
+        execution_receipt
+            .expect_commit(true)
+            .new_resource_addresses()
+    );
 }
