153e3b3fb1dc3f097dbaf43dd1e647cea05ee44c
Florian Pieper <florian@pieper.ai>
2023-09-10 11:25:41 +0200

    Update max supply

--- src/constants.rs
+++ src/constants.rs
@@ -1,4 +1,9 @@
 use scrypto::prelude::*;
 
-// MAX_SUPPLY = 1461501637330902918203684832716.283019655932542976
-pub const MAX_SUPPLY: Decimal = Decimal(BnumI256::from_digits([0, 0, 4294967296, 0]));
+// MAX_SUPPLY = 5708990770823839524233143877.797980545530986496
+pub const MAX_SUPPLY: Decimal = Decimal(I192::from_digits([0, 0, 16777216]));
+
+#[test]
+fn test_max_supply() {
+    assert_eq!(MAX_SUPPLY, Decimal(I192::from(2).pow(152)))
+}
